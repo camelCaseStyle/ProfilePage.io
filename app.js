d2d30da271373dbdb9867763bba90482c3a41694
Anubhav Ashish <anubhav.ashish9@gmail.com>
2019-01-06 00:50:11 +1100

    added fixes for layout and also extended home page to reflect some blog posts

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var express = require("express"),
-    app = express();
+    app = express()
+    mongoose = require("mongoose"),
+    ejsLint = require("ejs-lint");
 
 
 var indexRoutes = require("./routes/index");
-
+var DATABASEURL = process.env.DATABASEURL;
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
-
-var PORT = process.env.PORT,
-    IP = process.env.IP ;
+mongoose.connect(DATABASEURL, { useNewUrlParser: true });
+var PORT = process.env.PORT ,
+    IP = process.env.IP;
 
 
 app.listen(PORT, IP, function(){
     console.log("The server is up!")
 });
+
+
 
 app.use(indexRoutes); 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require("express"),
-    router = express.Router({mergeParams:true});
+    router = express.Router({mergeParams:true}),
+    Story = require("../models/story.js");
 
 router.get("/", function(req, res){
-    res.render("home/index");
+    Story.find({}, function(error, stories){
+        if(error){ 
+            console.log("Error !" + error);
+        }else{
+            res.render("home/index",{stories: stories});
+        }
+    });   
 });
 
 module.exports = router;
